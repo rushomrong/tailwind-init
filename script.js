2146ffc490e26fdac963163f2a7b527e9a4f0e32
rushomrong <rushomarak@gmail.com>
2024-08-11 03:49:56 +0600

    Tailwind sign in page created

---
+++ script.js
@@ -0,0 +1,6 @@
+let mobile_icn = document.getElementById("mobile_icn");
+let main_menu = document.getElementById("main_menu");
+
+mobile_icn.addEventListener("click", () => {
+  main_menu.classList.toggle("hidden");
+});
